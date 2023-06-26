9bec59f8ab84c7263b497fd62e466d53aecab714
popobo <331361996@qq.com>
2023-06-26 15:49:39 +0800

    initial commit

---
+++ extract_gcode/main.py
@@ -0,0 +1,60 @@
+import re
+
+class Point:
+    x = 0
+    y = 0
+
+def add_space():
+    str = ""
+    with open("常用汉字库3500.txt", "r", encoding="utf8") as file:
+        for line in file:
+            chars = list(line)
+            i = 0
+            for char in chars[1:]:
+                str += char
+                str += "  "
+                i += 1
+                if i == 8:
+                    str += "\n"
+                    i = 0
+    with open("char_with_space.txt", "w", encoding="utf8") as file:
+        file.write(str)
+
+def remove_two_lines(str):
+    lines = str.split('\n')
+    del lines[-3:]
+    str = '\n'.join(lines)
+    return str
+
+
+
+def extract_gcode():
+    pattern_xy = r"X([+-]?\d+(\.\d+)?)Y([+-]?\d+(\.\d+)?)"
+    pattern_z = r"G1G90 Z([+-]?\d+(\.\d+)?)F8000"
+    last_x_pos = 100
+    with open("第1页.gcode", "r") as file:
+        space_index = 0
+        line_index = 0
+        start_copy = False
+        result = ""
+        for line in file:
+            line_index += 1
+            if start_copy:
+                result = result + line
+            match_xy = re.search(pattern_xy, line)
+            if match_xy:
+                x_pos = float(match_xy.group(1))
+                if space_index == 0:
+                    start_copy = True
+                if (x_pos - last_x_pos > 10):
+                    space_index += 1
+                    print("line:", line_index, "space_index:", space_index)
+                    if start_copy:
+                        start_copy = False
+                        result = remove_two_lines(result)
+                last_x_pos = x_pos
+        
+
+if __name__ == "__main__":
+    # add_space()
+    extract_gcode()
